510b757a6caae96f285f558168105e13df4095f8
h41xy <h41@wafthrudnir>
2014-11-23 13:01:43 +0100

    Updated puppet browser installation to use the puppet apt module

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -4,11 +4,11 @@
 $tools = ["git" , "ruby", "python"]
 package { $tools: ensure => "installed" }
 
+include googlechrome
 # Browsers
 package { 'firefox':
   ensure => 'latest'
 }
-include googlechrome
 # Opera Repository currently broken (13.11.14)
 #include opera
 
@@ -22,30 +22,31 @@ include googlechrome
 #package { 'docker':
 #  ensure => latest,
 #}
-include docker
+#include docker
 
 ## apache docker container
 
-docker::image{'php':
-image_tag => 'apache',
-require => CLASS['docker'],
-}
+#docker::image{'php':
+#  image_tag => 'apache',
+#  require => CLASS['docker'],
+#}
 ## mysql docker container
-docker::image{'mysql':
-require => CLASS['docker']
-}
+#docker::image{'mysql':
+#  require => CLASS['docker']
+#}
 
-docker::run { 'mysql':
-  image => 'mysql',
-  use_name => true,
+#docker::run { 'mysql':
+#  image => 'mysql',
+#  use_name => true,
   # Must be set, otherwise SQL server wont run
-  env => 'MYSQL_ROOT_PASSWORD=abc'
-}
-->
-docker::run { 'webServer':
-  image => 'php:apache',
-  use_name => true,
+#  env => 'MYSQL_ROOT_PASSWORD=abc'
+#}
+#->
+#docker::run { 'webServer':
+#  image => 'php:apache',
+#  use_name => true,
   #ports => '80',
   #expose => '80',
-  links => ['mysql:db'],
-}
+#  links => ['mysql:db'],
+#  volumes => '/var/www',
+#}
--- puppet/modules/opera/manifests/init.pp
+++ puppet/modules/opera/manifests/init.pp
@@ -1,41 +1,69 @@
-# Install the opera browser in Ubunut 14.04
+## Install the opera browser in Ubunut 14.04
+#class opera (
+#  $dest_file_key = '/tmp/opera_key',
+#  $url_key = 'http://deb.opera.com/archive.key',
+#  $url_repo = 'http://deb.opera.com/opera/ stable non-free',
+#  $sources_file = '/etc/apt/sources.list.d/opera.list'
+#) {
+#  # Get the Key 
+#  exec { "get_key_opera":
+#    command => "wget -q -O ${dest_file_key} ${url_key}",
+#    path => $::path,
+#    creates => "${dest_file_key}"
+#  }
+#  
+#  # Install the key
+#  exec { "install_key_opera":
+#    command => "apt-key add ${dest_file_key}",
+#    path => $::path,
+#    require => EXEC['get_key_opera'],
+#    unless => "apt-key list | grep Opera"
+#  }
+#  
+#  # Add repository
+#  exec { "add_repository_opera":
+#    command => "echo \"deb ${url_repo}\" >> ${sources_file}",
+#    path => $::path,
+#    unless => "grep ${url_repo} ${sources_file}"
+#  }
+#  
+#  # Update repository list
+#  exec { "update_repository_opera":
+#    command => "apt-get update",
+#    path => $::path,
+#    require => EXEC['install_key_opera', 'add_repository_opera'],
+#  }
+#
+#  package { 'opera':
+#    ensure => 'latest',
+#    require => EXEC['update_repository_opera'],
+#  }
+#}
+################################################################################
+#
+#Install Opera Bowser with the puppet apt module
+#Repository still broken (23.11.2014)
 class opera (
-  $dest_file_key = '/tmp/opera_key',
   $url_key = 'http://deb.opera.com/archive.key',
-  $url_repo = 'http://deb.opera.com/opera/ stable non-free',
-  $sources_file = '/etc/apt/sources.list.d/opera.list'
-) {
-  # Get the Key 
-  exec { "get_key_opera":
-    command => "wget -q -O ${dest_file_key} ${url_key}",
-    path => $::path,
-    creates => "${dest_file_key}"
+  $url_repo = 'http://deb.opera.com/opera/',
+){
+  include apt
+
+  apt_key { 'opera_key':
+    source => $url_key,
+    # Last 8 digits of Fingerprint
+    id => 'A8492E35',
+    ensure => 'present',
   }
-  
-  # Install the key
-  exec { "install_key_opera":
-    command => "apt-key add ${dest_file_key}",
-    path => $::path,
-    require => EXEC['get_key_opera'],
-    unless => "apt-key list | grep Opera"
-  }
-  
-  # Add repository
-  exec { "add_repository_opera":
-    command => "echo \"deb ${url_repo}\" >> ${sources_file}",
-    path => $::path,
-    unless => "grep ${url_repo} ${sources_file}"
-  }
-  
-  # Update repository list
-  exec { "update_repository_opera":
-    command => "apt-get update",
-    path => $::path,
-    require => EXEC['install_key_opera', 'add_repository_opera'],
+
+  apt::source { 'opera_repository':
+    location => $url_repo,
+    release => 'stable',
+    repos => 'non-free',
+    include_src => false,
   }
 
   package { 'opera':
     ensure => 'latest',
-    require => EXEC['update_repository_opera'],
   }
 }
